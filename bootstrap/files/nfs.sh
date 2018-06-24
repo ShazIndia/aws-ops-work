5cbd664ab374b6c95c800705d91d2f60bbaf4792
Shashi Ranjan <shashi.ranjan2@thermofisher.com>
2018-06-24 23:16:39 +0530

    Add Server bootsrap recipe

--- bootstrap/files/nfs.sh
+++ bootstrap/files/nfs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Bootstarp script for installing and configuring  lvm and nfs for data filler and nfsfiller
+#Bootstarp script using chef recipes
 
 
 echo "Checking for directory"
 echo
-if [ -d '/shared/apps' ] && [ -d '/shared/data' ] && [ -d '/shared/home' ] && [ -d '/shared/lucene' ]; then
+if [ -d '/shared/apps-1' ] && [ -d '/shared/apps-2' ] && [ -d '/shared/apps-3' ] && [ -d '/shared/home' ]; then
     echo "Required Directory apps, data, home, lucene exists."
 
 else
@@ -23,17 +23,6 @@ sudo apt-get -y install lvm2
 #Install NFS server 
 sudo apt-get -y install nfs-kernel-server
 
-
-#edit /etc/export 
-sudo cat >/etc/export <<EOL
-/mnt01/exports/data *(rw,no_root_squash)
-/mnt02/exports/home *(rw,no_root_squash)
-/mnt02/exports/lucene *(rw,no_root_squash)
-/mnt02/exports/apps *(rw,no_root_squash)
-EOL
-
-#configure exports 
-sudo exportfs -a
 
 #restart the nfs  
 sudo service nfs-kernel-server start
